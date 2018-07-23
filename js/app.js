5c4f5879fe7f780c047251fd0a42b68e01f45e49
Prabald <36597875+Prabald@users.noreply.github.com>
2018-07-23 17:15:09 +0530

    Finished

--- js/app.js
+++ js/app.js
@@ -4,25 +4,16 @@
 
 let cards=["fa fa-diamond","fa fa-paper-plane-o","fa fa-anchor","fa fa-bolt","fa fa-cube","fa fa-anchor","fa fa-leaf","fa fa-bicycle","fa fa-diamond","fa fa-bomb","fa fa-leaf",
 "fa fa-bomb","fa fa-bolt","fa fa-bicycle","fa fa-paper-plane-o","fa fa-cube"];
-let start;
-let end
 let matched=0;
 let stars=3;
 let matchCount=0;
 let moveCount=0;
 let openCards=[];
-/*
- * To display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+
 //To create deck
    var timer = new Timer();
 
-
-
-
+//To create the deck and add event listeners to all the cards
 function createDeck(){
     cards=shuffle(cards);   
 
@@ -175,7 +166,6 @@ function shuffle(array) {
 
     return array;
 }
-
 /*Checks if the card is already open or matched*/
 function check(card) 
 {
@@ -196,15 +186,3 @@ function hasWon(){
     else 
         return false;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
